daea4d4b6f532b743e591c5b0eeed207cf303e0d
Joel Medina <jmedina@admins-MBP.fios-router.home>
2025-07-15 19:18:55 -0400

    Add tarball extract and pip install to startup.sh

--- startup.sh
+++ startup.sh
@@ -2,9 +2,18 @@
 
 # Log startup steps
 echo "===== RUNNING STARTUP.SH =====" >> /home/LogFiles/startup.log
+echo "Running from $(pwd)" >> /home/LogFiles/startup.log
 
-# Install dependencies
-echo "Installing pip and requirements..." >> /home/LogFiles/startup.log
+# Extract tarball if it exists
+if [ -f /home/site/wwwroot/output.tar.gz ]; then
+  echo "Extracting output.tar.gz..." >> /home/LogFiles/startup.log
+  tar -xzf /home/site/wwwroot/output.tar.gz -C /home/site/wwwroot >> /home/LogFiles/startup.log 2>&1
+else
+  echo "No output.tar.gz found." >> /home/LogFiles/startup.log
+fi
+
+# Install pip and requirements
+echo "Installing/upgrading pip and packages..." >> /home/LogFiles/startup.log
 python3 -m pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
 pip install -r /home/site/wwwroot/requirements.txt >> /home/LogFiles/startup.log 2>&1
 
