4691a4f5995f7eb73396f9929cf7dad178138413
Joel Medina <jmedina@admins-MBP.fios-router.home>
2025-07-25 14:43:30 -0400

    Update CORS and API_URL for production deployment

--- app/main.py
+++ app/main.py
@@ -34,9 +34,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         # your local test page (commented out for live deployment)
-        "http://localhost:5000",
-        "http://localhost:5500",          
-        "http://127.0.0.1:5500",
+        # "http://localhost:5000",
+        # "http://localhost:5500",          
+        # "http://127.0.0.1:5500",
+        "https://app.moreyummy.com",
         "https://bankagent.moreyummy.com",
         "https://moreyummy.com"
     ],
