fecdc90ce54d8c87b57c330ee61c46147dad6954
Joel Medina <jmedina@admins-MBP.fios-router.home>
2025-07-25 20:51:47 -0400

    Fix: ensure gunicorn install and launch in startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 echo "===== RUNNING STARTUP.SH =====" >> /home/LogFiles/startup.log
 echo "Running from $(pwd)" >> /home/LogFiles/startup.log
+echo "PATH is: $PATH" >> /home/LogFiles/startup.log
 
 # Extract tarball if it exists
 if [ -f /home/site/wwwroot/output.tar.gz ]; then
@@ -13,12 +14,19 @@ else
   echo "No output.tar.gz found." >> /home/LogFiles/startup.log
 fi
 
-# Attempt to use system-wide pip3 and python3
-echo "Installing packages from requirements.txt..." >> /home/LogFiles/startup.log
-python3 -m pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
-python3 -m pip install -r /home/site/wwwroot/requirements.txt >> /home/LogFiles/startup.log 2>&1
+# Activate virtual environment
+echo "Activating venv..." >> /home/LogFiles/startup.log
+source /tmp/8ddcbd08fb37067/antenv/bin/activate >> /home/LogFiles/startup.log 2>&1
 
-# Launch Gunicorn
+# Reinstall just to be safe
+echo "Installing packages from requirements.txt..." >> /home/LogFiles/startup.log
+pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
+pip install -r /home/site/wwwroot/requirements.txt >> /home/LogFiles/startup.log 2>&1
+
+# Log gunicorn path
+echo "Which gunicorn: $(which gunicorn)" >> /home/LogFiles/startup.log
+
+# Start gunicorn
 echo "Starting Gunicorn..." >> /home/LogFiles/startup.log
 exec gunicorn app.main:app \
   --workers 1 \
